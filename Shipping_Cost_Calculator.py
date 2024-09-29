561519cd2e64be86d7755997526e5b3a0248724b
najilil14 <najilil14@gmail.com>
2024-09-29 04:09:48 -0400

    Revert "another change"
    This reverts commit 048c7d88e62c1550d9b486d2f2247dbd2d72c6d5.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by najilil14
-
- # Here is another update by najilil14
-
